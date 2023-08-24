50e89182a63ceb2191da0df9a4ab9f98f6f3b834
Lian Canio <138495779+ChiaxNiro@users.noreply.github.com>
2023-08-23 21:44:52 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declarar las variables
+        int numero1 = 5;
+        int numero2 = 4;
+
+        // Realizar la suma
+        int resultado = numero1 + numero2;
+
+        // Mostrar el resultado en pantalla
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+}
